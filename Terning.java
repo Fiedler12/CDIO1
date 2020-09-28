bbc5b8306c2086d4931bad9e3457cff77a72f8b4
Fiedler12 <71757895+Fiedler12@users.noreply.github.com>
2020-09-28 14:15:18 +0200

    Add files via upload

---
+++ Terning.java
@@ -0,0 +1,31 @@
+package com.company;
+public class Terning {
+
+    int terning1;  //Første terning
+    int terning2;  // Anden terning
+
+    public Terning (){   //roll funktion
+        roll();
+        
+    }
+
+    public void roll(){  //Hvad skal denne roll funktion gøre?
+        terning1 = (int)(Math.random()*6+1);
+        terning2 = (int)(Math.random()*6+1);
+        
+    }
+    public int hentterning1() {
+        return terning1;
+
+    }
+    public int hentterning2() {
+        return terning2;
+
+    }
+    public int henttotal() {
+        return terning1 + terning2;
+                
+    }
+
+
+}
