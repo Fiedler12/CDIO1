dd684ec73d5f097afda8d19e4cbf26c50c589819
Andreas <s205450@student.dtu.dk>
2020-09-29 13:35:43 +0200

    Tilføjede tekst i main

--- Terningprogram/src/com/company/Main.java
+++ Terningprogram/src/com/company/Main.java
@@ -36,7 +36,7 @@ public class Main {
         System.out.println("Tryk enter for at slå.");
         input.nextLine();
         terning.roll();
-        System.out.println("Du har slået: " + terning.hentterning1() + " og " + terning.hentterning2() + " i alt har du slået:" + terning.henttotal());
+        System.out.println("Du har slået: " + terning.hentterning1() + " og " + terning.hentterning2() + " i alt har du slået: " + terning.henttotal());
         currentPlayer.setPoint(currentPlayer.getPoint() + terning.henttotal());
         System.out.println(currentPlayer.getNavn() + " har nu: " + currentPlayer.getPoint() + " point");
     }
