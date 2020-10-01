7fa84c0993ef31d954dde0a49dcb26693e62664d
Alex <s201172@student.dtu.dk>
2020-10-01 13:54:16 +0200

    Tilføjet ekstraopgave 2 + rettet småfejl

--- Terningprogram/src/com/company/Tur.java
+++ Terningprogram/src/com/company/Tur.java
@@ -16,6 +16,26 @@ public class Tur {
             System.out.println("Fordi du har slået to 1'ere har du mistet alle dine point.");
         }
         System.out.println(currentPlayer.getNavn() + " har nu: " + currentPlayer.getPoint() + " point");
+        while (terning.hentterning1() == terning.hentterning2()) {
+            if (currentPlayer.getPoint() >= 40) {
+                break;
+            }
+            System.out.println("Du har slået 2 ens, så du får en tur til");
+            System.out.println("Tryk enter for at slå.");
+            input.nextLine();
+            terning.roll();
+            System.out.println("Du har slået: " + terning.hentterning1() + " og " + terning.hentterning2() + " i alt har du slået: " + terning.henttotal());
+            currentPlayer.setPoint(currentPlayer.getPoint() + terning.henttotal());
+            if(currentPlayer.getPoint() >= 40) {
+                System.out.println(currentPlayer.getNavn() + " har nu: " + currentPlayer.getPoint() + " point");
+                break;
+            }
+            if (terning.henttotal() == 2) {
+                currentPlayer.setPoint(0);
+                System.out.println("Fordi du har slået to 1'ere har du mistet alle dine point.");
+            }
+            System.out.println(currentPlayer.getNavn() + " har nu: " + currentPlayer.getPoint() + " point");
+        }
     }
 
 }
